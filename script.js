d403a8950ed89096a7fb74ccf3689b45f604a0c8
viktor4mkd <117035233+viktor4mkd@users.noreply.github.com>
2023-03-11 22:13:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function inflationCalculator() {
+
+    let inflationRate = document.querySelector('#inflationRate');
+    let money = document.querySelector('#money');
+
+    inflationRate = parseFloat(inflationRate.value);
+    money = parseFloat(money.value);
+
+    let years = document.querySelector('#years').value
+    years = parseFloat(years);
+
+    let worth = money + (money * (inflationRate / 100));
+
+    for (let i = 1; i < years; i++) {
+
+        worth += worth * (inflationRate / 100);
+
+    }
+
+    let newElement = document.createElement('div');
+    newElement.className = 'new-value';
+    newElement.innerText = `Denesnite ${money}evra vredat isto kako  ${worth} za ${years} godini.`;
+    document.querySelector('.container').appendChild(newElement);
+
+}
+
+
